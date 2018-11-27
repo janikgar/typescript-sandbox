46cfb3300bf60c0a43d32aac927ba8930db63239
Jacob Garcia <jacob.garcia@codewilling.com>
2018-11-27 16:38:59 -0600

    gulp bundle

--- proj/gulpfile.js
+++ proj/gulpfile.js
@@ -1,9 +1,26 @@
 var gulp = require("gulp");
-var ts = require("gulp-typescript");
-var tsProject = ts.createProject("tsconfig.json");
+var browserify = require("browserify");
+var source = require("vinyl-source-stream");
+var tsify = require("tsify");
+var paths = {
+    pages: ['src/*.html']
+};
+
+gulp.task("copy-html", () => {
+    return gulp.src(paths.pages)
+               .pipe(gulp.dest("dist"));
+});
 
 gulp.task("default", () => {
-    return tsProject.src()
-                    .pipe(tsProject())
-                    .js.pipe(gulp.dest("dist"));
+    return browserify({
+        basedir: '.',
+        debug: true,
+        entries: ['src/main.ts'],
+        cache: {},
+        packageCache: {}
+    })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest("dist"));
 });
