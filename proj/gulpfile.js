60031b826464e5b735fbba9c4a1950633e918402
Jacob Garcia <jacob.garcia@codewilling.com>
2018-11-27 17:03:24 -0600

    not fully working, but more gulp tasks

--- proj/gulpfile.js
+++ proj/gulpfile.js
@@ -1,26 +1,34 @@
 var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require("vinyl-source-stream");
-var tsify = require("tsify");
+var watchify = require("watchify");
+// var tsify = require("tsify");
+var fancylog = require("fancy-log");
 var paths = {
     pages: ['src/*.html']
 };
+
+var watchedBrowserify = watchify(browserify({
+    basedir: '.',
+    debug: true,
+    entries: ['src/main.ts'],
+    cache: {},
+    packageCache: {}
+}))
+
+function bundle() {
+    return watchedBrowserify.bundle()
+                            .pipe(source('bundle.js'))
+                            .pipe(gulp.dest("dist"));
+}
 
 gulp.task("copy-html", () => {
     return gulp.src(paths.pages)
                .pipe(gulp.dest("dist"));
 });
 
-gulp.task("default", () => {
-    return browserify({
-        basedir: '.',
-        debug: true,
-        entries: ['src/main.ts'],
-        cache: {},
-        packageCache: {}
-    })
-    .plugin(tsify)
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest("dist"));
-});
+// gulp.task("default", bundle);
+
+gulp.task("default", gulp.parallel("copy-html"), bundle);
+watchedBrowserify.on("update", bundle);
+watchedBrowserify.on("log", fancylog);
